a21a9fe322c691daa6d75b0d12a3a72940d33314
junyeonglee1111 <leejunyeong1111@gmail.com>
2025-06-26 18:33:16 +0900

    feat: Print the Sign of the Valuable n

---
+++ test01.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n % 2 == 0:
+    print("odd number")
+else:
+    print("even number")
+
+if n > 0:
+    print("positive number")
+elif n < 0:
+    print("negative number")
+else:
+    print("zero")
